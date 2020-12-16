1bafec387c3d3774764a30fb8cf35b2a6b5ceac3
Ethan Kovalenko <ashervoronin@gmail.com>
2020-12-16 15:36:26 -0600

    Add project files.

---
+++ Hello World 2.5 Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_2._5_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
